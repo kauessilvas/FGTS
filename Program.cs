0760484a61355375466e539961219ddf7671d984
kauessilvas <kauesantosds5@gmail.com>
2025-04-05 14:20:57 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+double salary, fgts;
+Console.WriteLine("============= Calcular FGTS =============");
+while (true)
+{
+    Console.Write("Salário (R$)..: ");
+    string input = Console.ReadLine()!;
+
+    if (double.TryParse(input, out salary))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Valor inválido! Por favor, digite um número.");
+    }
+}
+
+fgts = salary / 100 * 8;
+Console.WriteLine($"FGTS: {fgts:C2}");
+Console.WriteLine("=========================================");
